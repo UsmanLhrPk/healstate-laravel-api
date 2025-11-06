c69d12b253a6f2cc90c50b31373ed71d8663ff6f
Usman Javaid <usmanjzcn@gmail.com>
2025-11-07 01:20:03 +0500

    removed csrf protection from all routes

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,6 +12,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->validateCsrfTokens(except: [
+            '*', // Exclude all routes from CSRF verification
+        ]);
+
         $middleware->api(prepend: [
             \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
         ]);
