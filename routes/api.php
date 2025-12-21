7dc67f3c2863cd9a6aef91cd333104e18fd30440
umer-two-bit <umeralampklhr@gmail.com>
2025-12-21 17:12:31 +0500

    api endpoints added

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,29 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ForumController;
+use App\Http\Controllers\CommentController;
+use App\Http\Controllers\LikeController;
+use App\Http\Controllers\FlagController;
 
-Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
-    return $request->user();
+// Public routes (no auth required)
+Route::get('/forums', [ForumController::class, 'index']);
+Route::get('/forums/{id}', [ForumController::class, 'show']);
+Route::get('/comments', [CommentController::class, 'index']);
+
+// Protected routes (auth required)
+Route::middleware('auth:sanctum')->group(function () {
+    // Forums
+    Route::post('/forums', [ForumController::class, 'store']);
+    Route::delete('/forums/{id}', [ForumController::class, 'destroy']);
+
+    // Comments
+    Route::post('/comments', [CommentController::class, 'store']);
+    Route::delete('/comments/{id}', [CommentController::class, 'destroy']);
+
+    // Likes
+    Route::post('/likes', [LikeController::class, 'toggle']);
+
+    // Flags
+    Route::post('/flags', [FlagController::class, 'store']);
 });
