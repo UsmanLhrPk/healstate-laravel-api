a60f3fe3bc04efae2f2864174611f2e2f597fe72
umer-two-bit <umeralampklhr@gmail.com>
2025-12-25 22:17:36 +0500

    seeder conflict push

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,13 +21,6 @@ class DatabaseSeeder extends Seeder
             // Add any other seeders you have
         ]);
         
-        // Or if you just want to create test data quickly:
-        // User::factory(10)->create();
-        
-        // // Create a specific test user
-        // User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+      
     }
 }
