817711649dc90f898833775f2f7d6dd5e7c2244d
umer-two-bit <umeralampklhr@gmail.com>
2026-02-25 01:02:59 +0500

    Update the createSPAAxios function with the parse error fix

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,6 @@ Route::get('/', function () {
     return ['Laravel' => app()->version()];
 });
 
-require __DIR__.'/auth.php';
 
 Route::get('/test-log', function() {
     \Log::info('Test log message');
